a9da888f1486250967dc65f0b905de449301fe0e
slathas24 <48292537+slathas24@users.noreply.github.com>
2025-08-29 15:56:18 +0530

    Update test_openai_key.py

--- NEWHACK/test_openai_key.py
+++ NEWHACK/test_openai_key.py
@@ -1,7 +1,6 @@
 import openai
 
-OPENAI_API_KEY = "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"
-
+OPENAI_API_KEY = "sk-proj-"
 def test_openai_key():
     openai.api_key = OPENAI_API_KEY
     try:
@@ -14,3 +13,4 @@ def test_openai_key():
 
 if __name__ == "__main__":
     test_openai_key()
+
